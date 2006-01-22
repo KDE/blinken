1cde39b41af560df8f233dc75ae7738b0cef2f61
Laurent Montel <montel@kde.org>
2006-01-22 13:50:51 +0000

    Fix some compile error read*entry--
    svn path=/trunk/KDE/kdeedu/blinken/; revision=501227

--- src/button.cpp
+++ src/button.cpp
@@ -25,19 +25,19 @@ button::button(blinkenGame::color c) : m_selected(false), m_color(c)
 	switch (c)
 	{
 		case blinkenGame::blue:
-			m_key = kc->readNumEntry(cs, Qt::Key_3);
+			m_key = kc->readEntry(cs, int(Qt::Key_3));
 		break;
 		
 		case blinkenGame::yellow:
-			m_key = kc->readNumEntry(cs, Qt::Key_1);
+			m_key = kc->readEntry(cs, int(Qt::Key_1));
 		break;
 		
 		case blinkenGame::red:
-			m_key = kc->readNumEntry(cs, Qt::Key_2);
+			m_key = kc->readEntry(cs, int(Qt::Key_2));
 		break;
 		
 		case blinkenGame::green:
-			m_key = kc->readNumEntry(cs, Qt::Key_4);
+			m_key = kc->readEntry(cs, int(Qt::Key_4));
 		break;
 		
 		default:
