6eadcf347c93c04dd2e0bce41987525d5ace6997
Albert Astals Cid <tsdgeos@terra.es>
2007-09-09 21:22:42 +0000

    use non deprecated version
    svn path=/trunk/KDE/kdeedu/blinken/; revision=710402

--- src/fontchecker.cpp
+++ src/fontchecker.cpp
@@ -20,7 +20,7 @@ bool fontChecker::checkInstalled(const QFont &font, const QString &fontPath)
 	// Works with Steve may need some tweaking to work with other fonts
 	if (!fi.exactMatch())
 	{
-		bool success = KIO::NetAccess::file_copy(KUrl::fromPath(fontPath), KUrl("fonts:/Personal/"), 0);
+		bool success = KIO::NetAccess::file_copy(KUrl::fromPath(fontPath), KUrl("fonts:/Personal/"), (QWidget*)0);
 		return !success;
 	}
 	return true;
