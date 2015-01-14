943218d919be6e1d3701b4ae2cd5c01bb6fedb95
Albert Astals Cid <aacid@kde.org>
2015-01-15 00:09:32 +0100

    Increase version to 0.4
    May as well increase the version a bit :D

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	KLocalizedString::setApplicationDomain("blinken");
 
 	QApplication app(argc, argv);
-	KAboutData about("blinken", i18n("Blinken"), "0.3", i18n("A memory enhancement game"), KAboutLicense::GPL, i18n("© 2005-2007 Albert Astals Cid\n© 2005-2007 Danny Allen"));
+	KAboutData about("blinken", i18n("Blinken"), "0.4", i18n("A memory enhancement game"), KAboutLicense::GPL, i18n("© 2005-2007 Albert Astals Cid\n© 2005-2007 Danny Allen"));
 	about.addAuthor(i18n("Albert Astals Cid"), i18n("Coding"), "aacid@kde.org");
 	about.addAuthor(i18n("Danny Allen"), i18n("Design, Graphics and Sounds"), "danny@dannyallen.co.uk");
 	about.addCredit(i18n("Steve Jordi"), i18n("GPL'ed his 'Steve' font so that we could use it"), "steve@sjordi.com");
